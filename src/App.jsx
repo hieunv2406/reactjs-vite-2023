a2ab21325dcf9a928aa59daf12292336ffa9612a
HaroldNguyen <HaroldNguyen>
2023-11-01 18:51:14 +0700

    init todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from './components/views/MyComponent'
+import MyComponent from './components/views/example/MyComponent'
+import ListTodo from './components/views/todo/ListTodo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +22,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button><br/>
-        <MyComponent/>
+        <MyComponent />
+        <ListTodo/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/views/todo/ListTodo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ListTodo extends React.Component {
+ 
+    render() {
+        return(<>List todo</>)
+    }
+}
+
+export default ListTodo;
